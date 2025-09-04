af06aa150289b2ec86b45aff920d66b73812ebfc
Davi_Mostaro <davicafezinho15@gmail.com>
2025-09-04 12:07:50 -0300

    feat: creating header general structure

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+
+export function Header() {
+    return (
+        <header className="bg-white border-b border-gray-200">
+            <div className="bg-black text-center text-white p-4">
+                <strong>FRETE GRÁTIS</strong> para todo o Sudeste nas compras acima de R$ 199,00. <strong>APROVEITA!</strong>
+            </div>
+            <div className="w-full max-w-6xl mx-auto p-6">
+                <div className="flex items-center justify-between">
+                    <div className="">
+                        <Image 
+                            src="/assets/ui/logo-black.png" 
+                            alt="B7Store"
+                            width={120}
+                            height={40}
+                        />
+                    </div>
+                    <div className="">
+                        Botões
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
