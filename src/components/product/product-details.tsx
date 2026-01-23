b85a484f0f21d4b6f3fa3b292455c2482f35957b
Davi_Mostaro <davicafezinho15@gmail.com>
2026-01-23 19:54:20 -0300

    feat: adding item to cart

--- src/components/product/product-details.tsx
+++ src/components/product/product-details.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { setCartState } from "@/actions/set-cart-state";
 import { useCartStore } from "@/store/cart";
 import { ProductComplete } from "@/types/product";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 type Props = {
     product: ProductComplete;
@@ -12,7 +14,10 @@ export const ProductDetails = ({ product }: Props) => {
     const cartStore = useCartStore(state => state);
 
     const addToCart = async () => {
-        //TODO: Criação do carrinho de compras
+        cartStore.addItem({ productId: product.id, quantity: 1 }); 
+        const updatedCart = useCartStore.getState().cart;
+        await setCartState(updatedCart);
+        redirect('/cart');
     }
 
     return(
